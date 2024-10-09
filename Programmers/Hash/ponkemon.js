e4fc9a5df584364baa9fb09d23b4aeb8922a8172
RohSungKyun <songkyun1108@naver.com>
2024-10-09 23:56:14 +0900

    programmers : 포켓몬2

--- Programmers/Hash/ponkemon.js
+++ Programmers/Hash/ponkemon.js
@@ -1,4 +1,10 @@
 function solution(nums) {
     var answer = 0;
+    const pick_max = nums.length/2;
+    for (let i =0; i < nums.length; i++){
+        if(answer < pick_max){
+            //
+        }
+    }
     return answer;
 }
