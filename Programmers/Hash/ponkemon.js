597c02a1b79e81f8ef3f538997ef36e6b09af4c3
RohSungKyun <songkyun1108@naver.com>
2024-10-10 15:16:53 +0900

    programmers : pokemon solution

--- Programmers/Hash/ponkemon.js
+++ Programmers/Hash/ponkemon.js
@@ -1,10 +1,9 @@
+// 최대로 고를 수 있는 포켓몬의 종류는 nums/2이다.
+// 최대한 많은 포켓몬을 고르는 경우의 수를 구하는 문제이다.
+
 function solution(nums) {
-    var answer = 0;
+    var answer = new Set(nums).size;
     const pick_max = nums.length/2;
-    for (let i =0; i < nums.length; i++){
-        if(answer < pick_max){
-            //
-        }
-    }
-    return answer;
+    
+    return answer < pick_max ? answer : pick_max;
 }
