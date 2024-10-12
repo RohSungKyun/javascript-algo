de979efce868fc093849839f490bec1d748d706e
RohSungKyun <songkyun1108@naver.com>
2024-10-12 19:30:23 +0900

    programmers : rectangle2

--- Programmers/Brute_Force/rectangle.js
+++ Programmers/Brute_Force/rectangle.js
@@ -3,10 +3,10 @@ function solution(sizes) {
     let max_w = 0;
     let max_h = 0;
     for (let i=0; i<sizes.length; i++){
-        let [width, height] = sizes[i].sort((a, b) => a - b)
+        const [width, height] = sizes[i].sort((a, b) => a - b)
         max_w = Math.max(max_w, width);
         max_h = Math.max(max_h, height);
-        answer = width*height;
+        answer = max_h*max_w;
     }
     
     return answer;
