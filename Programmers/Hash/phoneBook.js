5664bbf1d21f90ebd3be727740b78848914123b8
RohSungKyun <songkyun1108@naver.com>
2024-10-19 22:42:33 +0900

    programmers : 전화번호 목록 주석 추가

--- Programmers/Hash/phoneBook.js
+++ Programmers/Hash/phoneBook.js
@@ -1,8 +1,8 @@
 function solution(phone_book) {
     var answer = true;
-    phone_book.sort();
+    phone_book.sort(); // 짧은 문자열이 앞에 오도록 하기 위해 정렬
     for (let i =0; i<phone_book.length - 1; i++){
-        if(phone_book[i] === phone_book[i+1].substring(0, phone_book[i].length)){
+        if(phone_book[i] === phone_book[i+1].substring(0, phone_book[i].length)){ // startwith를 쓰기도 함.
             answer = false;
         }
     }
