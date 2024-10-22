9a571b15ee4f8af148b79f685f56d57a6d3fa534
RohSungKyun <songkyun1108@naver.com>
2024-10-22 15:37:19 +0900

    programmers : 의상 풀이

--- Programmers/Hash/clothes.js
+++ Programmers/Hash/clothes.js
@@ -1,5 +1,15 @@
 function solution(clothes) {
     var answer = 0;
+    // 배열을 순회하면서 맵에 저장
+    let map = new Map();
+    for (let [name, category] of clothes){
+        map.set(category, (map.get(category)||0)+1);
+    }
+    let combination = 1;
+    for (let value of map.values()){
+        combination = combination * (value + 1);
+    }
+
     
-    return answer;
+    return combination-1;
 }
