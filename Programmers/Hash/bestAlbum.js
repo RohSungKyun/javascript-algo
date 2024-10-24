9934634af713ad8271dd37535e289d0d9637cb7e
RohSungKyun <songkyun1108@naver.com>
2024-10-24 21:15:30 +0900

    programmers : 베스트 앨범1

--- Programmers/Hash/bestAlbum.js
+++ Programmers/Hash/bestAlbum.js
@@ -1,4 +1,10 @@
 function solution(genres, plays) {
     var answer = [];
+    let genresMap = new Map();
+    for (let i = 0;i<genres.length; i++){
+        //
+    }
+    
+    
     return answer;
 }
