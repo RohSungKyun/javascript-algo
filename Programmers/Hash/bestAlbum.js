fe3ad645e9f52e936a90dcfc6075dcf2c797d97c
RohSungKyun <songkyun1108@naver.com>
2024-10-25 23:55:32 +0900

    programmers : 베스트 앨범2

--- Programmers/Hash/bestAlbum.js
+++ Programmers/Hash/bestAlbum.js
@@ -1,10 +1,18 @@
+function getSortedGenres(map) {
+    const sortedGenres = [...map].sort((a, b) => a[1] - b[1]);
+    return sortedGenres;
+}
 function solution(genres, plays) {
     var answer = [];
     let genresMap = new Map();
     for (let i = 0;i<genres.length; i++){
-        //
+        if (!genresMap.get(genres[i])){
+            genresMap.set(genres[i], plays[i]);
+        } else {
+            genresMap.set(genres[i], genresMap.get(genres[i]) + plays[i]);
+        }
     }
-    
+    const sortedGenres = getSortedGenres(genresMap);
     
     return answer;
 }
