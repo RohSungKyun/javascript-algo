b4203d43edb4f2d57448b11cfc4a20b046db4c0b
RohSungKyun <songkyun1108@naver.com>
2024-10-27 01:07:42 +0900

    programmers : 베스트앨범 최적화

--- Programmers/Hash/bestAlbum.js
+++ Programmers/Hash/bestAlbum.js
@@ -1,29 +1,32 @@
-function getSortedGenres(map) {
-    const sortedGenres = [...map].sort((a, b) => b[1] - a[1]);
-    return sortedGenres;
-}
 function solution(genres, plays) {
-    var answer = [];
-    let genresMap = new Map();
-    const allAlbum = [];
-    for (let idx = 0; idx<genres.length; idx++){
-        let tmp =[genres[idx], plays[idx], idx]; // 장르, 재생수, 인덱스
-        allAlbum.push(tmp); // 모든 음악을 저장
-    }
-    for (let i = 0;i<genres.length; i++){
-        if (!genresMap.get(genres[i])){
-            genresMap.set(genres[i], plays[i]);
-        } else {
-            genresMap.set(genres[i], genresMap.get(genres[i]) + plays[i]);
-        }
-    }
-    const sortedGenres = getSortedGenres(genresMap);
-    for (let genre of sortedGenres){
-        let count = 0;
-        for (let album of allAlbum){
-            if (genre === album[0]){ // 같은 장르일 경우
-                
+    const answer = [];
+    const genreMap = new Map(); // 장르별 총 재생 수와 앨범 정보 저장
+    
+    for (let i=0; i<genres.length; i++){
+        let genre = genres[i];
+        let play = plays[i];
+        
+        if (!genreMap.has(genres[i])){
+            genreMap.set(genres[i], {totalPlay:0, albums:[]});
+        }; // 장르, 전체 재생 횟수, 각 앨범 정보 배열
+        
+        genreMap.get(genres[i]).totalPlay += play;
+        genreMap.get(genres[i]).albums.push([play, i]);
+    };
+    const sortedGenres = [...genreMap.entries()].sort((a, b) => {
+        return b[1].totalPlay - a[1].totalPlay
+    }); // map에는 정렬기능이 없으므로 entries를 통해 배열로 만들어 정렬
+
+    for (let [genre, {albums}] of sortedGenres) {
+        albums.sort((a, b) => {
+            if(a[0] === b[0]){
+                return a[1] - b[1];
+            } else{
+                return b[0] - a[0];
             }
+        });
+        for (let i=0; i<Math.min(2, albums.length); i++){
+            answer.push(albums[i][1]);
         }
     }
     
