d33e1a581c6d219e8d9195e443424cf5375ce973
RohSungKyun <songkyun1108@naver.com>
2024-10-26 23:56:26 +0900

    programmers : 모든 앨범 저장하는 배열 추가

--- Programmers/Hash/bestAlbum.js
+++ Programmers/Hash/bestAlbum.js
@@ -1,10 +1,15 @@
 function getSortedGenres(map) {
-    const sortedGenres = [...map].sort((a, b) => a[1] - b[1]);
+    const sortedGenres = [...map].sort((a, b) => b[1] - a[1]);
     return sortedGenres;
 }
 function solution(genres, plays) {
     var answer = [];
     let genresMap = new Map();
+    const allAlbum = [];
+    for (let idx = 0; idx<genres.length; idx++){
+        let tmp =[genres[idx], plays[idx], idx]; // 장르, 재생수, 인덱스
+        allAlbum.push(tmp); // 모든 음악을 저장
+    }
     for (let i = 0;i<genres.length; i++){
         if (!genresMap.get(genres[i])){
             genresMap.set(genres[i], plays[i]);
@@ -13,6 +18,14 @@ function solution(genres, plays) {
         }
     }
     const sortedGenres = getSortedGenres(genresMap);
+    for (let genre of sortedGenres){
+        let count = 0;
+        for (let album of allAlbum){
+            if (genre === album[0]){ // 같은 장르일 경우
+                
+            }
+        }
+    }
     
     return answer;
 }
