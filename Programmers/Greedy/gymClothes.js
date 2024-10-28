05e5586127a394e2434c037762b505cab03038c9
RohSungKyun <songkyun1108@naver.com>
2024-10-29 00:15:57 +0900

    programmers : 체육복 문제 해결

--- Programmers/Greedy/gymClothes.js
+++ Programmers/Greedy/gymClothes.js
@@ -1,5 +1,19 @@
 function solution(n, lost, reserve) {
-    var answer = n-lost.length;
+    const barrowable = lost.filter(l => !reserve.includes(l)).sort((a, b) => a - b);
+    const canReserve = reserve.filter(r => !lost.includes(r)).sort((a, b) => a - b);
+    let answer = n - barrowable.length;
+    
+    for (let i = 0; i < barrowable.length; i++){
+        let lostNumber = barrowable[i];
+        for (let j = 0; j < canReserve.length; j++){
+            let reserveNumber = canReserve[j];
+            if(reserveNumber === lostNumber-1 || reserveNumber === lostNumber + 1){
+                answer +=1;
+                canReserve[j] = -1;
+                break;
+            }
+        }
+    }
     
     return answer;
 }
