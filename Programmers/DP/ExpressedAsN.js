0ea1e86be10d00f46a1425aff1dcc42b6b8ef8d0
RohSungKyun <songkyun1108@naver.com>
2024-10-31 23:33:09 +0900

    programmers : dp 기능구현 필요

--- Programmers/DP/ExpressedAsN.js
+++ Programmers/DP/ExpressedAsN.js
@@ -2,6 +2,10 @@ function solution(N, number) {
     var answer = 0;
     const use = Array.from(new Array(9), () => new Set());
     
+    if(N==number) return 1;
+    else {
+        // 기능 구현
+    }
     
     return answer;
 }
