a19dbab79dad21dfe4e71adf667b4ae82a1be0ea
RohSungKyun <songkyun1108@naver.com>
2024-11-22 23:48:09 +0900

    programmers : 더맵게 최소 힙 조건

--- Programmers/Heap/moreSpicy.js
+++ Programmers/Heap/moreSpicy.js
@@ -1,3 +1,11 @@
+// getParentIndex: 부모 노드의 인덱스를 반환합니다.
+// getLeftChildIndex: 왼쪽 자식 노드의 인덱스를 반환합니다.
+// getRightChildIndex: 오른쪽 자식 노드의 인덱스를 반환합니다.
+// swap: 두 노드의 값을 교환합니다.
+// push: 새로운 요소를 힙에 삽입합니다.
+// pop: 힙에서 가장 작은 요소(루트 노드)를 꺼내고 제거합니다.
+// isEmpty: 힙이 비어있는지 확인합니다.
+
 function solution(scoville, K) {
     var answer = 0;
     return answer;
