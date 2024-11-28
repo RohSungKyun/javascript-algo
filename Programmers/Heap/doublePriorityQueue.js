dfaadb8a15ba3b61db241e46c25ae35802b586c6
RohSungKyun <songkyun1108@naver.com>
2024-11-28 23:12:45 +0900

    programmers : 이중우선순위큐 아이디어

--- Programmers/Heap/doublePriorityQueue.js
+++ Programmers/Heap/doublePriorityQueue.js
@@ -1,3 +1,18 @@
+// I 숫자 : 큐에서 주어진 숫자를 삽입한다.
+// D1 : 큐에서 최댓값을 삭제한다.
+// D-1 : 큐에서 최솟값을 삭제한다.
+// 결과값으로 큐가 비어있으면 [0,0]을, 비어있지 않으면 [최댓값, 최솟값]을 return 하도록 구현
+// 아이디어1 : 최소힙, 최대힙을 한 클래스 안에 동시에 구현하고 하나의 힙을 공유한다.
+
+class Heap {
+    constructor() {
+        this.heap = [];
+    }
+
+
+
+}
+
 function solution(operations) {
     var answer = [];
     return answer;
