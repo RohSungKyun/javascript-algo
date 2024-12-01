14d304881cb3b03a76a9b9379c32c348947341ce
RohSungKyun <songkyun1108@naver.com>
2024-12-01 23:19:05 +0900

    programmers : 최소힙 삽입 구현

--- Programmers/Heap/doublePriorityQueue.js
+++ Programmers/Heap/doublePriorityQueue.js
@@ -39,7 +39,38 @@ class Heap {
             let parentIndex = this.getParentIndex(currentIndex);
         }
     }
+    isEmpty() {
+        return this.heap.length === 0; // 힙이 비어있으면 true를 반환
+    }
+    // pop 메서드는 힙에서 가장 작은 요소를 꺼내고 제거
+    pop() {
+        if (this.isEmpty()) return null; // 힙이 비어있을때
+        if (this.heap.length === 1) return this.heap.pop(); // 힙에 요소가 하나밖에 없다면 요소 제거 후 반환
 
+        const root = this.heap[0]; // 루트 노드를 변수에 저장하고 마지막 요소를 루트로 이동
+        this.heap[0] = this.heap.pop();
+
+        let currentIndex = 0;
+        let leftChildindex = this.getLeftChildIndex(currentIndex);
+        let rightChildIndex = this.getRightChildIndex(currentIndex);
+
+        // 현재 노드의 값이 왼쪽 자식이나 오른쪽 자식보다 크다면 교환
+        while(
+            (leftChildindex < this.heap.length && this.heap[currentIndex] > this.heap[leftChildindex]) ||
+            (rightChildIndex < this.heap.length && this.heap[currentIndex] > this.heap[rightChildIndex])
+        ) {
+            // 좌우 자식 중 더 작은 값과 교환
+            const smallerIndex = rightChildIndex >= this.heap.length || this.heap[leftChildindex] < this.heap[rightChildIndex]
+            ? leftChildindex : rightChildIndex;
+            this.swap(currentIndex, smallerIndex);
+
+            currentIndex = smallerIndex;
+            leftChildindex = this.getLeftChildIndex(currentIndex);
+            rightChildIndex = this.getRightChildIndex(currentIndex);
+        }
+        return root;
+
+    }
 
 }
 
