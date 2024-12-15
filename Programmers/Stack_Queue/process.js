076ecc9010f11a642a30de49755118d6cbb32764
RohSungKyun <songkyun1108@naver.com>
2024-12-15 22:31:02 +0900

    programmers : 프로세스 해결

--- Programmers/Stack_Queue/process.js
+++ Programmers/Stack_Queue/process.js
@@ -1,30 +1,67 @@
 class Queue{
-  constructor(){
-    this.queue = [];
+    constructor(){
+      this.queue = [];
+    }
+    add(value) {
+      this.queue.push(value);
+    }
+    popleft(){
+      return this.queue.shift();
+    }
+  
+    peek() {
+      return this.queue[0];
+    }
+  
+    getSize() {
+      return this.queue.length;
+    }
+  
+    isEmpty() {
+      if (this.getSize() === 0){
+        return true;
+      }
+      return false;
+    }
+    isFirst(value){ //인자보다 우선순위가 큰 값이 있는지 큐를 탐색
+      for(let process of this.queue){
+        if(value < process){
+          return false;
+        }
+      }
+      return true;
+    }
   }
-  add(value) {
-    this.queue.push(value);
-  }
-  popleft(){
-    return this.queue.shift();
-  }
-
-  peek() {
-    return this.queue[0];
-  }
-
-  getSize() {
-    return this.queue.length;
-  }
-
-  isEmpty() {
-    return this.getSize() === 0;
-  }
-}
-
-function solution(priorities, location) {
-    const queue = new Queue();
-    var answer = 0;
-
+  
+  function solution(priorities, location) {
+    const prioritieQueue = new Queue();
+    const elementQueue = new Queue();
+    let answer = 1;
+    let target;
+  
+    for(let prioritie of priorities){
+      prioritieQueue.add(prioritie);
+    }
+    for(let i = 0; i<prioritieQueue.getSize(); i++){
+      elementQueue.add(i);
+      if (i === location){
+        target = i;
+      }
+    }
+  
+    while(prioritieQueue.getSize() !== 0){
+      let process = prioritieQueue.popleft(); //우선순위
+      let item = elementQueue.popleft(); //해당 우선순위의 값
+        
+      if (prioritieQueue.isFirst(process)){ //큰 값이 없으면
+        if (item === target){
+          return answer;
+        }
+        answer+=1;
+      } else{
+        prioritieQueue.add(process);
+        elementQueue.add(item);
+      }
+    }
     return answer;
-}
+  }
