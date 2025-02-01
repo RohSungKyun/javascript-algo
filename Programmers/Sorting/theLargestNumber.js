ffb6b4e5e1d0e6dd29592231e03de7421750e4f9
RohSungKyun <songkyun1108@naver.com>
2025-02-01 23:54:45 +0900

    programmers : 가장 큰 수 그리디로 해결

--- Programmers/Sorting/theLargestNumber.js
+++ Programmers/Sorting/theLargestNumber.js
@@ -1,16 +1,21 @@
-// 가장 큰 수 를 만들려면 맨 앞에 오는 각 숫자의 첫번재 숫자는 가장 큰 수여야함.
-// 이를 반복, 문자열로 처리 필요
-
 function solution(numbers) {
-    let maxValue = '0';
-    var answer = '';
-    for (let number of numbers) {
-      if (String(number).substring(0, 1) > maxValue.substring(0, 1)) {
-        
+  numbers = numbers.map(String);
+  let answer = '';
+
+  while (numbers.length > 0) {
+    let maxNum = '0';
+    let maxIdx = -1;
+
+    numbers.forEach((num, idx) => {
+      if (maxNum + num < num + maxNum) {
+        maxNum = num;
+        maxIdx = idx;
       }
-      for (let i=0; i<number.length; i++) {
-        
-      }
-    }
-    return answer;
+    });
+
+    answer += maxNum;
+    numbers.splice(maxIdx, 1); // 선택한 숫자 제거
+  }
+
+  return answer[0] === '0' ? '0' : answer;
 }
