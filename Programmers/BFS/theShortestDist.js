a0b76be1f84761ff938d112565d0c8931c75e89f
RohSungKyun <songkyun1108@naver.com>
2025-02-08 23:16:54 +0900

    programmers : bfs테스트 케이스 통과

--- Programmers/BFS/theShortestDist.js
+++ Programmers/BFS/theShortestDist.js
@@ -7,29 +7,32 @@ function solution(maps) {
 }
 
 function bfs(graph) {
-    const visited = Array.from({length: 5}, () => Array(5).fill(false));
-    const dx = [0, 1, -1, 0];
-    const dy = [1, 0, 0, -1];
+    const N = graph.length; // 하드코딩하지말고 유연하게 선언
+    const M = graph[0].length;    
+    const visited = Array.from({length: N}, () => Array(M).fill(false));
+    const dx = [0, 1, 0, -1]; // 방향 코드는 우, 하, 좌, 상으로 순서대로 작성
+    const dy = [1, 0, -1, 0];
     const queue = [];
-    queue.push([0, 0, 0]); // x, y, distance
+    queue.push([0, 0, 1]); // x, y, distance
     visited[0][0] = true;
     
-    while (queue > 0) {
+    while (queue.length > 0) {
+
         const [x, y, distance] = queue.shift();
         
-        if(x === 4 && y === 4) {
+        if(x === N-1 && y === M-1) {
             return distance;
         }
         for(let i=0; i<dx.length; i++) {
             let nx = x + dx[i];
             let ny = y + dy[i];
             if (
-                nx>=0 && nx<5 &&
-                ny>=0 && ny<5 &&
+                nx>=0 && nx < N &&
+                ny>=0 && ny < M &&
                 visited[nx][ny] !== true &&
                 graph[nx][ny] !== 0
             ) {
-                queue.push([nx, ny, distance+=1]);
+                queue.push([nx, ny, distance+1]);
                 visited[nx][ny] = true;
             }
         }
