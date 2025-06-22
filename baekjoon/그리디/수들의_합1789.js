9f7add38e97c2d462077f17fefbb52bbe630e62d
RohSungKyun <songkyun1108@naver.com>
2025-06-23 00:02:27 +0900

    bj 1789수들의 누적 합

--- baekjoon/그리디/수들의_합1789.js
+++ baekjoon/그리디/수들의_합1789.js
@@ -1,4 +1,14 @@
 // 서로 다른 n개의 자연수의 합이 s 일 때, 자연수 n의 최댓값
-// 개수와 더하는 수 중 최댓값이 같음.
 
 const s = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+let sum = 0;
+let count = 0;
+
+for (let i = 1; i <= s; i++) {
+  if (sum + i > s) break;
+  sum += i;
+  count++;
+}
+
+console.log(count);
